a957ba2a007c2369622735c4a27bde01ecff8800
Hugo de Lelis <hugodelelis00@gmail.com>
2024-09-26 15:11:14 -0300

    test responsive

--- src/app/modules/pages/index/index.component.ts
+++ src/app/modules/pages/index/index.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { HomeComponent } from '../../components/home/home.component';
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NavComponent, HomeComponent ],
+  imports: [NavComponent, HomeComponent, AboutComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })

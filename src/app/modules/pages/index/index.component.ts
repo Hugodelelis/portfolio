85a47a7f9b3c42c754abb81e85da2142246f0f74
Hugo de Lelis <hugodelelis00@gmail.com>
2024-09-26 15:51:02 -0300

    nav

--- src/app/modules/pages/index/index.component.ts
+++ src/app/modules/pages/index/index.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { HomeComponent } from '../../components/home/home.component';
 import { AboutComponent } from '../../components/about/about.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NavComponent, HomeComponent, AboutComponent, FooterComponent],
+  imports: [NavComponent, HomeComponent, AboutComponent, FooterComponent, ProjectsComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })

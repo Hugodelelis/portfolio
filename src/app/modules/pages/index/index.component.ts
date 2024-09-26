447b1ec42455384331d1a508855d718406eaa4fb
Hugo de Lelis <hugodelelis00@gmail.com>
2024-09-26 15:27:06 -0300

    add footer

--- src/app/modules/pages/index/index.component.ts
+++ src/app/modules/pages/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { HomeComponent } from '../../components/home/home.component';
 import { AboutComponent } from '../../components/about/about.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NavComponent, HomeComponent, AboutComponent],
+  imports: [NavComponent, HomeComponent, AboutComponent, FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
